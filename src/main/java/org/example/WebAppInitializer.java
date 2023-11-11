bdaa306c22c576f22e20c6dda1aed790f8a1547f
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-11-11 20:27:13 +0530

    Create WebAppInitializer.java

---
+++ src/main/java/org/example/WebAppInitializer.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.example.config.WebAppConfig;
+import org.example.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 8:08 PM
+ * 2023-11-11 - 11 - 2023
+ */
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(new MultipartConfigElement(System.getProperty("java.io.tmpdir")));
+    }
+}
